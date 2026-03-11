28588f1a9f6b294d07defbfd7dd08c725c1ca417
Luiz Silvino <luizsilvino03@gmail.com>
2026-03-11 11:32:05 -0300

    Create atOpcional.js

---
+++ JS - Senai/Atividades - Senai/atOpcional.js
@@ -0,0 +1,27 @@
+let nome = "João";
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 6;
+let media
+
+media = (nota1 + nota2 + nota3 ) / 3;
+let resultado
+
+if (true) {
+
+    if (media >= 6) {
+        resultado = "Aprovado"
+    } else {
+        resultado = "Reprovado"
+    }
+
+}
+
+console.log("Aluno:", nome)
+console.log("Nota 1:", nota1)
+console.log("Nota 2:", nota2)
+console.log("Nota 3:", nota3)
+
+console.log("Média:", media)
+
+console.log("Resultado:", "Aprovado")
