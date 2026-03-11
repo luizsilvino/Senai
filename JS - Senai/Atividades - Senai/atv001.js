0408af0e42d2c0b10088f2346d88ff638e7b1b88
Luiz Silvino <luizsilvino03@gmail.com>
2026-03-11 11:04:09 -0300

    Create atv001.js

---
+++ JS - Senai/Atividades - Senai/atv001.js
@@ -0,0 +1,25 @@
+let nome = "João";
+let nota1 = 8;
+let nota2 = 7;
+let media
+
+media = (nota1 + nota2) / 2;
+let resultado
+
+if (true) {
+
+    if (media >= 6) {
+        resultado = "Aprovado"
+    } else {
+        resultado = "Reprovado"
+    }
+
+}
+
+console.log("Aluno:", nome)
+console.log("Nota 1:", nota1)
+console.log("Nota 2:", nota2)
+
+console.log("Média:", media)
+
+console.log("Resultado:", "Aprovado")
